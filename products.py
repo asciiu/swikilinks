1bd808f991859bf2440209b6f16fb48ca3f624f6
Flowmaster <ellyssin.gimhae@gmail.com>
2018-10-26 17:45:11 -0600

    Added function for parent sku extraction.

--- products.py
+++ products.py
@@ -1,39 +1,7 @@
 import csv
+import re
 
-#file = "products_export.csv"
-#f = open(file)
-#csv_file = csv.reader(f)
-#
-#sku_index = 0
-#desc_index = 0
-#title_index = 0
-#
-#title = ""
-#
-#for i, val in enumerate(csv_file):
-#
-#  # first row should be headers
-#  if i == 0:
-#    # assign indices of columns based upon headers
-#    for j, header in enumerate(val):
-#      if header == "Variant SKU":
-#        sku_index = j
-#      if header == "Option1 Value":
-#        desc_index = j
-#      if header == "Title":
-#        title_index = j
-#     # skip outer loop once since the first row are headers
-#  else:
-#    # if the title is empty in title cell then use the last title
-#    title = title if val[title_index] == "" else val[title_index]
-#
-#    sku = val[sku_index]
-#    desc = val[desc_index]
-#
-#    print title + " (" + val[sku_index] + "): " + val[desc_index] 
-#
-#f.close()
-
+# parses the products export csv from shopify
 def ParseProductsFile(file):
     f = open(file)
     csv_file = csv.reader(f)
@@ -68,3 +36,32 @@ def ParseProductsFile(file):
 
     f.close()
     return products
+
+# extracts the product from the sku
+# e.g. RLMBFV extracted from RLMBFV8
+def ExtractParentSku(sku):
+  # if RLTEGU just return the sku
+  if "RLTEGU" in sku:
+    return sku
+
+  # if RL5050 return RL5050
+  if sku.startswith("RL5050"):
+    return "RL5050" 
+
+  # if sku ends with MICRO return the first part only
+  if sku.endswith("MICRO"):
+    return sku[:-len("MICRO")]
+
+  # if the sku ends with MINI return the first part only
+  if sku.endswith("MINI"):
+    return sku[:-len("MINI")]
+    
+  # extract the parent code before the first digit
+  parts = re.match(r"([a-z]+)", sku, re.I) 
+  # if there are parts 
+  if parts:
+    # return the first part before the number
+    return parts.group(1)
+  else: 
+    # no match just returns the entire sku
+    return sku
